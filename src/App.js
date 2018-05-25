9099c701c5926a5f275e476cebbc7b2d323262f5
Jeremias Erbs <jerry@badabam.de>
2018-05-25 12:22:25 +0200

    Add CounterButton to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ToggleButton from './components/ToggleButton'
+import CounterButton from './components/CounterButton'
 import styled from 'react-emotion'
 
 const Grid = styled('section')`
@@ -14,10 +15,12 @@ class App extends Component {
     this.state = {
       habits: [
         { text: 'Sport gemacht', checked: false, id: 0 },
-        { text: '2 Liter getrunken', checked: false, id: 1 },
+        { text: 'Liter getrunken', count: 0, id: 1 },
         { text: 'Gesund gegessen', checked: false, id: 2 },
         { text: 'Abends gelernt', checked: false, id: 3 },
         { text: 'Im Kurs beteiligt', checked: false, id: 4 },
+        { text: 'Zigaretten geraucht', count: 0, id: 5 },
+        { text: 'Fragen im Kurs gestellt', count: 0, id: 6 },
       ],
     }
   }
@@ -36,17 +39,64 @@ class App extends Component {
     this.setState({ habits: newHabits })
   }
 
+  increaseCount(id) {
+    const allHabits = this.state.habits
+    const habitIndex = allHabits.findIndex(habit => habit.id === id)
+    const oldHabit = allHabits[habitIndex]
+
+    const newHabits = [
+      ...allHabits.slice(0, habitIndex),
+      { ...oldHabit, count: oldHabit.count + 1 },
+      ...allHabits.slice(habitIndex + 1),
+    ]
+
+    this.setState({ habits: newHabits })
+  }
+
+  decreaseCount(id) {
+    const allHabits = this.state.habits
+    const habitIndex = allHabits.findIndex(habit => habit.id === id)
+    const oldHabit = allHabits[habitIndex]
+
+    // check if it is already 0
+    if (oldHabit.count === 0) {
+      return // stop here, if it is already 0
+    }
+
+    const newHabits = [
+      ...allHabits.slice(0, habitIndex),
+      { ...oldHabit, count: oldHabit.count },
+      ...allHabits.slice(habitIndex + 1),
+    ]
+
+    this.setState({ habits: newHabits })
+  }
+
   render() {
     return (
       <Grid>
-        {this.state.habits.map(habit => (
-          <ToggleButton
-            text={habit.text}
-            checked={habit.checked}
-            key={habit.id}
-            onClick={e => this.toggleHabit(habit.id)}
-          />
-        ))}
+        {this.state.habits.map(habit => {
+          if (habit.checked != null) {
+            return (
+              <ToggleButton
+                text={habit.text}
+                checked={habit.checked}
+                key={habit.id}
+                onClick={e => this.toggleHabit(habit.id)}
+              />
+            )
+          } else if (habit.count != null) {
+            return (
+              <CounterButton
+                text={habit.text}
+                count={habit.count}
+                key={habit.id}
+                onIncrease={e => this.increaseCount(habit.id)}
+                onDecrease={e => this.decreaseCount(habit.id)}
+              />
+            )
+          }
+        })}
       </Grid>
     )
   }
