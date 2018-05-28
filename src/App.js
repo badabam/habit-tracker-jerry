4787c2b789714721b52010046bf8c0693f1a1392
Jeremias Erbs <jerry@badabam.de>
2018-05-28 23:33:39 +0200

    Move state to App again.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,93 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import moment from 'moment'
+
+import globalStyles from './styles/global'
+import habits from './data/habits'
+
 import Today from './components/Today'
 import History from './components/History'
 import Settings from './components/Settings'
 
+globalStyles()
+
 class App extends Component {
+  state = {
+    habits: habits,
+    dayOffset: 0,
+    history: {},
+  }
+
+  updateHistory(id, updateFunction) {
+    const oldEntries = this.state.history[this.currentDate] || {}
+    const oldValue = oldEntries[id]
+
+    const updatedEntries = {
+      ...oldEntries,
+      [id]: updateFunction(oldValue),
+    }
+    this.setState({
+      history: {
+        ...this.state.history,
+        [this.currentDate]: updatedEntries,
+      },
+    })
+  }
+
+  toggleHabit = id => {
+    this.updateHistory(id, oldValue => !oldValue)
+  }
+
+  increaseCount = id => {
+    this.updateHistory(id, oldValue => (oldValue == null ? 1 : oldValue + 1))
+  }
+
+  decreaseCount = id => {
+    this.updateHistory(
+      id,
+      oldValue => (oldValue == null || oldValue === 0 ? 0 : oldValue - 1)
+    )
+  }
+
+  get currentDate() {
+    return moment()
+      .add(this.state.dayOffset, 'days')
+      .format('DD.MM.YYYY')
+  }
+
+  moveDayLeft = () => {
+    this.setState(state => ({
+      dayOffset: state.dayOffset - 1,
+    }))
+  }
+
+  moveDayRight = () => {
+    this.setState({
+      dayOffset: this.state.dayOffset + 1,
+    })
+  }
+
   render() {
     return (
       <Router>
         <section>
-          <Route exact path="/" component={Today} />
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <Today
+                habits={this.state.habits}
+                data={this.state.history}
+                dayOffset={this.state.dayOffset}
+                currentDate={this.currentDate}
+                moveDayLeft={this.moveDayLeft}
+                moveDayRight={this.moveDayRight}
+                toggleHabit={this.toggleHabit}
+                increaseCount={this.increaseCount}
+                decreaseCount={this.decreaseCount}
+              />
+            )}
+          />
           <Route path="/history" component={History} />
           <Route path="/settings" component={Settings} />
           <div>
--- src/components/Today.js
+++ src/components/Today.js
@@ -1,68 +1,10 @@
 import React, { Component } from 'react'
 import { css } from 'emotion'
-import moment from 'moment'
 
-import habits from '../data/habits'
-import globalStyles from '../styles/global'
 import HabitList from './HabitList'
 import DateSwitch from './DateSwitch'
 
-// inject global styles
-globalStyles()
-
 class Today extends Component {
-  state = {
-    habits: habits,
-    dayOffset: 0,
-    history: {},
-  }
-
-  updateHistory(id, updateFunction) {
-    const oldEntries = this.state.history[this.currentDate] || {}
-    const oldValue = oldEntries[id]
-
-    const updatedEntries = {
-      ...oldEntries,
-      [id]: updateFunction(oldValue),
-    }
-    this.setState({
-      history: { ...this.state.history, [this.currentDate]: updatedEntries },
-    })
-  }
-
-  toggleHabit = id => {
-    this.updateHistory(id, oldValue => !oldValue)
-  }
-
-  increaseCount = id => {
-    this.updateHistory(id, oldValue => (oldValue == null ? 1 : oldValue + 1))
-  }
-
-  decreaseCount = id => {
-    this.updateHistory(
-      id,
-      oldValue => (oldValue == null || oldValue === 0 ? 0 : oldValue - 1)
-    )
-  }
-
-  get currentDate() {
-    return moment()
-      .add(this.state.dayOffset, 'days')
-      .format('DD.MM.YYYY')
-  }
-
-  moveDayLeft = () => {
-    this.setState(state => ({
-      dayOffset: state.dayOffset - 1,
-    }))
-  }
-
-  moveDayRight = () => {
-    this.setState({
-      dayOffset: this.state.dayOffset + 1,
-    })
-  }
-
   render() {
     const dateSwitchStyles = css`
       position: absolute;
@@ -71,30 +13,42 @@ class Today extends Component {
       right: 0;
     `
 
+    const {
+      currentDate,
+      moveDayLeft,
+      moveDayRight,
+      toggleHabit,
+      increaseCount,
+      decreaseCount,
+      habits,
+      data: history,
+      dayOffset,
+    } = this.props
+
     return (
       <React.Fragment>
         <DateSwitch
           className={dateSwitchStyles}
-          text={this.currentDate}
-          onLeft={this.moveDayLeft}
-          onRight={this.moveDayRight}
-          isToday={this.state.dayOffset === 0}
+          text={currentDate}
+          onLeft={moveDayLeft}
+          onRight={moveDayRight}
+          isToday={dayOffset === 0}
         />
         <HabitList
           headline="Gut"
-          habits={this.state.habits.filter(habit => habit.category === 'good')}
-          data={this.state.history[this.currentDate]}
-          onToggle={this.toggleHabit}
-          onIncrease={this.increaseCount}
-          onDecrease={this.decreaseCount}
+          habits={this.props.habits.filter(habit => habit.category === 'good')}
+          data={history[currentDate]}
+          onToggle={toggleHabit}
+          onIncrease={increaseCount}
+          onDecrease={decreaseCount}
         />
         <HabitList
           headline="Schlecht"
-          habits={this.state.habits.filter(habit => habit.category === 'bad')}
-          data={this.state.history[this.currentDate]}
-          onToggle={this.toggleHabit}
-          onIncrease={this.increaseCount}
-          onDecrease={this.decreaseCount}
+          habits={habits.filter(habit => habit.category === 'bad')}
+          data={history[currentDate]}
+          onToggle={toggleHabit}
+          onIncrease={increaseCount}
+          onDecrease={decreaseCount}
         />
       </React.Fragment>
     )
