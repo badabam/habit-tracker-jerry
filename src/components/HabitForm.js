80d91676491f39e107b4c819379d2159effcfafe
Jeremias Erbs <jerry@badabam.de>
2018-05-29 20:31:54 +0200

    Improve HabitForm styling

--- src/components/HabitForm.js
+++ src/components/HabitForm.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react'
 import SwitchButton from './SwitchButton'
 import uid from 'uid'
+import styled from 'react-emotion'
+import { Box } from 'grid-emotion'
+
+const StyledInput = styled('input')`
+  height: 40px;
+  border: 1px solid #ddd;
+  box-shadow: inset 2px 2px 4px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  width: 100%;
+`
+
+const StyledButton = styled('section')`
+  display: flex;
+  font-family: sans-serif;
+  align-items: center;
+  justify-content: center;
+  height: 40px;
+  background: skyblue;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`
 
 export default class HabitForm extends Component {
   state = {
@@ -36,26 +57,32 @@ export default class HabitForm extends Component {
 
   render() {
     return (
-      <div>
-        <input
-          value={this.state.habitText}
-          onChange={e => this.setState({ habitText: e.target.value })}
-          type="text"
-          placeholder="Add name here"
-        />
-        <SwitchButton
-          textLeft="gut"
-          textRight="schlecht"
-          onClick={this.onCategorySelected}
-          selectedIndex={this.state.selectedCategory}
-        />
-        <SwitchButton
-          textLeft="Toggle"
-          textRight="Counter"
-          onClick={this.onTypeSelected}
-          selectedIndex={this.state.selectedType}
-        />
-        <button onClick={this.onCreateHabit}>Create</button>
+      <div style={{ padding: 10 }}>
+        <Box mb={2}>
+          <StyledInput
+            value={this.state.habitText}
+            onChange={e => this.setState({ habitText: e.target.value })}
+            type="text"
+            placeholder="Add name here"
+          />
+        </Box>
+        <Box mb={3}>
+          <SwitchButton
+            textLeft="gut"
+            textRight="schlecht"
+            onClick={this.onCategorySelected}
+            selectedIndex={this.state.selectedCategory}
+          />
+        </Box>
+        <Box mb={3}>
+          <SwitchButton
+            textLeft="Toggle"
+            textRight="Counter"
+            onClick={this.onTypeSelected}
+            selectedIndex={this.state.selectedType}
+          />
+        </Box>
+        <StyledButton onClick={this.onCreateHabit}>Create</StyledButton>
       </div>
     )
   }
--- src/components/SwitchButton.js
+++ src/components/SwitchButton.js
@@ -1,4 +1,31 @@
 import React, { Component } from 'react'
+import styled from 'react-emotion'
+
+const StyledContainer = styled('section')`
+  display: flex;
+  font-family: sans-serif;
+  height: 40px;
+`
+
+const HalfButton = styled('button')`
+  overflow: hidden;
+  width: 100%;
+  background: ${props => (props.disabled ? 'skyblue' : '#eee')};
+  font-weight: ${props => (props.disabled ? 'bold' : 'normal')};
+  box-shadow: ${p =>
+    p.disabled ? 'none' : 'inset 1px 1px 4px rgba(0, 0, 0, 0.1)'};
+  color: black;
+  border-radius: 4px;
+  border: none;
+  font-size: 1.1em;
+
+  &:first-child {
+    border-radius: 4px 0 0 4px;
+  }
+  &:last-child {
+    border-radius: 0 4px 4px 0;
+  }
+`
 
 export default class SwitchButton extends Component {
   render() {
@@ -6,22 +33,14 @@ export default class SwitchButton extends Component {
     const selectedIndex = this.props.selectedIndex || 0
 
     return (
-      <div>
-        <button
-          disabled={selectedIndex === 0}
-          style={{ background: selectedIndex === 0 ? '#555' : 'white' }}
-          onClick={e => onClick(0)}
-        >
+      <StyledContainer>
+        <HalfButton disabled={selectedIndex === 0} onClick={e => onClick(0)}>
           {textLeft}
-        </button>
-        <button
-          disabled={selectedIndex === 1}
-          style={{ background: selectedIndex === 1 ? '#555' : 'white' }}
-          onClick={e => onClick(1)}
-        >
+        </HalfButton>
+        <HalfButton disabled={selectedIndex === 1} onClick={e => onClick(1)}>
           {textRight}
-        </button>
-      </div>
+        </HalfButton>
+      </StyledContainer>
     )
   }
 }
